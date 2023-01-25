1954ac5cf483cf0a52e213263559ca3c48660c1d
ChristoferMendes <christoferluizdsm@gmail.com>
2023-01-25 11:05:46 -0300

    posts crud, i.e, controller, service & repository

---
+++ src/posts/repositories/posts.repository.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePostDto } from '../dto/create-post.dto';
+import { UpdatePostDto } from '../dto/update-post.dto';
+
+@Injectable()
+export class PostsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createPostDto: CreatePostDto) {
+    return this.prisma.post.create({
+      data: createPostDto,
+    });
+  }
+
+  findAll() {
+    return this.prisma.post.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.post.findUniqueOrThrow({ where: { id } });
+  }
+
+  update(id: number, updatePostDto: UpdatePostDto) {
+    return this.prisma.post.update({
+      where: { id },
+      data: updatePostDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.post.delete({ where: { id } });
+  }
+}
