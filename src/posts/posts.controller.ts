bf1e7d2c785c59f0cfb6b293895e6bbb5306307c
ChristoferMendes <christoferluizdsm@gmail.com>
2023-01-25 16:06:16 -0300

    added a new line in the ApiTags

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -11,6 +11,7 @@ import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { ApiTags } from '@nestjs/swagger';
+
 @ApiTags('Posts')
 @Controller('posts')
 export class PostsController {
