f46f6e6af9b1ebbbd2a7c211f2d763e749304db0
ChristoferMendes <christoferluizdsm@gmail.com>
2023-01-25 15:58:29 -0300

    swagger setup done

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({ description: 'Title of the post' })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({ description: 'Content of the post ' })
   @IsString()
   @IsOptional()
   content?: string;
 
+  @ApiProperty({ description: 'Email of the author' })
   @IsEmail()
   authorEmail: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { generateString } from 'src/shared/autoGenString';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'User name', default: 'John Doe' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'User email',
+    default: `GENERATEDEMAIL${generateString(4)}@gmail.com`,
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    description: 'Define if the user is admin',
+    default: false,
+  })
   @IsBoolean()
   admin: boolean;
 }
